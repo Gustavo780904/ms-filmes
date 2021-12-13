2a94148475142e6103f58906307eb91648e16b40
Gustavo780904 <gustavodutrasantos@gmail.com>
2021-12-13 17:28:30 -0300

    Usando bean para criar uma instancia RestTemplate para o Movie Catalog

--- movie-catalog-service/src/main/java/com/iftm/moviecatalogservice/resources/CatalogResource.java
+++ movie-catalog-service/src/main/java/com/iftm/moviecatalogservice/resources/CatalogResource.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,12 +18,15 @@ import com.iftm.moviecatalogservice.models.Rating;
 @RequestMapping("/catalog")
 public class CatalogResource {
 
+	@Autowired
+	private RestTemplate restTemplate;
+
 	@RequestMapping("/{userId}")
 	public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
 
-		List<Rating> ratings = Arrays.asList(new Rating("12", 5), new Rating("15", 2));
-
-		RestTemplate restTemplate = new RestTemplate();
+		List<Rating> ratings = Arrays.asList(new Rating("12", 15), new Rating("15", 20));
+		
+//		UserRating ratings = restTemplate.getForObject("http://localhost:8083/ratingsdata/users/" + userId, UserRating.class);
 
 		return ratings.stream().map(rating -> {
 			Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
